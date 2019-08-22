0a715828f93e1918a442baad6badb649e873ba4b
张世阳 <ZHANGSHIYANG162@pingan.com.cn>
2019-08-22 15:55:49 +0800

    master:[update]

--- libs/ecs/input.ts
+++ libs/ecs/input.ts
@@ -1,16 +1,19 @@
 import { ECS } from './ecs';
 
+export type CodeMap = {
+    [key: string]: number;
+};
 /** 输入组件 */
 export class Input {
     // ecs
     protected _ecs: ECS;
-    private _codeMap = new Map();
+    private _codeMap: Map<number, string> = new Map();
 
     // code 集合
-    private _keyCode = {};
+    private _keyCode: CodeMap = {};
 
     // 输入集合
-    private _inputMap = new Map();
+    private _inputMap: Map<number, any> = new Map();
     constructor(ecs: ECS) {
         this._ecs = ecs;
     }
@@ -19,14 +22,11 @@ export class Input {
      * 设置keycode
      * @param (object) obj keycode
      */
-    set keyCode(obj) {
-        const keyCode = typeof obj === 'string' ? JSON.parse(obj) : obj;
+    set keyCode(code_map: CodeMap) {
+        this._keyCode = code_map;
 
-        this._keyCode = keyCode;
-
-        for (const key of Object.keys(keyCode)) {
-            const code = keyCode[key];
-
+        for (const key of Object.keys(code_map)) {
+            const code = code_map[key];
             this._codeMap.set(code, key);
         }
     }
@@ -44,7 +44,7 @@ export class Input {
      * @param (string) code keycode
      * @return (object) data 输入数据
      */
-    public get(code) {
+    public get(code: string) {
         const data = this._inputMap.get(code);
 
         this.remove(code);
@@ -65,7 +65,7 @@ export class Input {
      * @param (string) code keycode
      * @param (object) data 输入数据
      */
-    public set(code, data) {
+    public set(code: number, data) {
         if (!this._codeMap.has(code)) {
             console.warn('Key Code does not exist using the name');
 
